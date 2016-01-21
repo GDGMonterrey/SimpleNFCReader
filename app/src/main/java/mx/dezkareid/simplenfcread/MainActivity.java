9fb0b5b7764a4a0cf2bccfb76b55c97a1a2c437b
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2016-01-21 00:44:06 -0600

    Metodos para descubrir el tag

--- app/src/main/java/mx/dezkareid/simplenfcread/MainActivity.java
+++ app/src/main/java/mx/dezkareid/simplenfcread/MainActivity.java
@@ -1,29 +1,52 @@
 package mx.dezkareid.simplenfcread;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NfcAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adapter = NfcAdapter.getDefaultAdapter(this);
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+        if (!adapterWorks()){
+            return;
+        }
+        handler(intent);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        adapter.disableForegroundDispatch(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    public boolean adapterWorks(){
+        if(adapter == null){
+            return false;
+        }
+        return adapter.isEnabled();
+    }
+
+
+    private void handler(Intent intent) {
+        String action = intent.getAction();
+        if ( NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+        }
     }
 }
