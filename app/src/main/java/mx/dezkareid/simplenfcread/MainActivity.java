74af5349b209931c51127dcc9e791bdc22460c52
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2016-01-20 22:07:13 -0600

    Sobreescribiendo metodos

--- app/src/main/java/mx/dezkareid/simplenfcread/MainActivity.java
+++ app/src/main/java/mx/dezkareid/simplenfcread/MainActivity.java
@@ -1,5 +1,6 @@
 package mx.dezkareid.simplenfcread;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
