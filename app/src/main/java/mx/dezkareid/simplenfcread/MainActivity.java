2b5cecff0bcd4c6565082b21d1ce2e5402ae8c96
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2016-01-27 01:45:21 -0600

    Se agrego metodo para convertir el contenido del ndef en texto y metodo agregado para desplegar contenido usando toast

--- app/src/main/java/mx/dezkareid/simplenfcread/MainActivity.java
+++ app/src/main/java/mx/dezkareid/simplenfcread/MainActivity.java
@@ -9,7 +9,10 @@ import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -86,11 +89,30 @@ public class MainActivity extends AppCompatActivity {
         short tnf = ndefRecord.getTnf();
         if (tnf == NdefRecord.TNF_WELL_KNOWN){
             if(Arrays.equals(NdefRecord.RTD_TEXT, ndefRecord.getType())){
+                String content = getTextFromRecord(ndefRecord);
+                Log.d("PARCELABLE", content);
+                showMessage(content);
 
             }
 
         }
-
     }
 
+    private String getTextFromRecord(NdefRecord ndefRecord){
+        String tagContent = null;
+        try {
+            byte[] payload = ndefRecord.getPayload();
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+            int languageSize = payload[0] & 0063;
+            tagContent = new String(payload,languageSize+1,
+                    payload.length -languageSize -1,textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("TextFromRecord", e.getMessage(), e);
+        }
+        return tagContent;
+    }
+
+    private void showMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_LONG);
+    }
 }
