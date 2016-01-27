add3a638a610f0bc942bf6872073c5c4d9c53d00
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2016-01-27 01:02:32 -0600

    Agregado disable y enable Foreground,junto con handler de intents

--- app/src/main/java/mx/dezkareid/simplenfcread/MainActivity.java
+++ app/src/main/java/mx/dezkareid/simplenfcread/MainActivity.java
@@ -1,9 +1,13 @@
 package mx.dezkareid.simplenfcread;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        if(!adapterWorks())
+            return;
+        enableSystemForegroundNFC();
     }
 
     public boolean adapterWorks(){
@@ -43,10 +50,21 @@ public class MainActivity extends AppCompatActivity {
         return adapter.isEnabled();
     }
 
-
+    private void enableSystemForegroundNFC(){
+        Intent intent = new Intent(this,this.getClass());
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+        IntentFilter[] intentFilter = new IntentFilter[]{new IntentFilter(adapter.ACTION_TAG_DISCOVERED) };
+        adapter.enableForegroundDispatch(this,pendingIntent,intentFilter,null);
+    }
     private void handler(Intent intent) {
         String action = intent.getAction();
         if ( NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+            extractParcelableInfo(intent.getParcelableArrayExtra(adapter.EXTRA_NDEF_MESSAGES));
         }
+    }
+
+    private void extractParcelableInfo( Parcelable [] parcelables){
+        Log.d("PARCELABLE", parcelables.length + "");
     }
 }
